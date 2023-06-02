f5ea909a43afedc476e6d05aed44705714937e19
MasterOfCats <120806919+TheMasterOfCats@users.noreply.github.com>
2023-06-02 16:56:59 +0200

    Add files via upload

---
+++ Code.ino
@@ -0,0 +1,14 @@
+int liczbapina=9;
+void setup() {
+pinMode(liczbapina,OUTPUT);
+}
+void loop() {
+analogWrite(liczbapina,145);
+delay(1350);
+analogWrite(liczbapina,0);
+delay(1350);
+analogWrite(liczbapina,255);
+delay(1350);
+analogWrite(liczbapina,75);
+delay(2000);
+}
